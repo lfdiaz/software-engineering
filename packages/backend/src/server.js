101fe828fd4c509b49d9e765daa84f25b02afa50
lfdiaz <your_email_address@example.com>
2020-04-18 16:45:39 -0500

    Modified start script

--- packages/backend/src/server.js
+++ packages/backend/src/server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 app.use(express.json());
+const path = require("path");
 
 const PORT = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
@@ -11,5 +12,7 @@ mongoose.connect(DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", error => console.log(`Database error: ${error}`));
 db.once("open", () => console.log("connected to database"));
+
+// app.use(express.static(path.join(__dirname, "../../ui/public")));
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
